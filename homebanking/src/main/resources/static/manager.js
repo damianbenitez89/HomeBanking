621aafef5dfe3bd5549834f77af9569184442e67
damianbenitez89 <78812666+damianbenitez89@users.noreply.github.com>
2021-11-13 15:18:38 -0300

    Front!

---
+++ homebanking/src/main/resources/static/manager.js
@@ -0,0 +1,22 @@
+let url = 'api/clients'
+
+fetch(url)
+.then(response=> response.json())
+.then(data=>{
+
+    let table= document.getElementById('table')
+
+    data.forEach(cliente => {
+        let row = document.createElement("tr");
+        row.innerHTML=`
+        
+            <th scope="row">${cliente.firstName}</th>
+            <td>${cliente.lastName}</td>
+            <td>@${cliente.email}</td>
+        `
+        table.appendChild(row)
+    });
+    
+})
+
+.catch(err=>console.log(err))
